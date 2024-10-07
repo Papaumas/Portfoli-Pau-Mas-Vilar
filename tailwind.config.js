d132b36354e59bade8a4683763ac69cbcd88be51
Papaumas <paumas123@gmail.com>
2024-10-07 10:41:19 +0200

    Esbos portfoli

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'default-bg': '#ECEBF7',
+      },
+      fontFamily: {
+        noto: ['Noto Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
